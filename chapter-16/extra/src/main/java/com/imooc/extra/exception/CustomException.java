795024aeb05b481611933eb1941fd9d15883d6ea
LewisZ11 <zhangyibo607@gmail.com>
2022-10-16 23:03:42 -0700

    First Demo of my project
    I would like to contribute my first demo project.

---
+++ chapter-16/extra/src/main/java/com/imooc/extra/exception/CustomException.java
@@ -0,0 +1,12 @@
+package com.imooc.extra.exception;
+
+/**
+ * <h1>CustomException</h1>
+ * Created by Qinyi.
+ */
+public class CustomException extends Exception {
+
+    public CustomException(String message) {
+        super(message);
+    }
+}
